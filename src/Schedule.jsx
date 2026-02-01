e0efb82293fbcfcabd36a90effd2a1352e9b8f26
dummarj-maker <dummarj@gmail.com>
2026-02-01 10:51:26 -0700

    Delete src directory

--- src/Schedule.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Schedule(){
-  return (
-    <div className="card">
-      <h2>Schedule</h2>
-      <p className="mono">Round-robin generator will populate matchups here.</p>
-    </div>
-  )
-}
--- src/Standings.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Standings(){
-  return (
-    <div className="card">
-      <h2>Standings</h2>
-      <p className="mono">W-L-T plus total points. Tiebreakers: points, H2H.</p>
-    </div>
-  )
-}
