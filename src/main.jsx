2e505b0f4f21bf1f495163b57e6b0e21d8928cb0
dummarj-maker <dummarj@gmail.com>
2026-02-01 17:27:26 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import Home from "./Home.jsx";
 import "./styles.css";
 
-console.log("✅ main.jsx loaded");
+const rootEl = document.getElementById("root");
 
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
