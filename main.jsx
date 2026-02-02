4cc519e0dcfb2f9a0fa98b5182975b8f70f87f24
dummarj-maker <dummarj@gmail.com>
2026-02-01 16:33:31 -0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import Home from "./Home.jsx";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
